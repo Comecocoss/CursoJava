302a2e38bfb67758f51c7b81824d780e9a29705f
corre <corre@192.168.0.38>
2022-05-03 10:32:56 -0300

    Primer commit

---
+++ src/main/java/Variables.java
@@ -0,0 +1,17 @@
+
+
+
+public class Variables {
+    public static void main(String[] args) {
+        int pizzas = 6;
+        int personas = 3;
+        int PizzasPorPersonas = pizzas/personas;
+        
+        System.out.printf("Si hay %d pizzas y %d personas, a cada uno le tocan %d pizzas\n", pizzas, personas, PizzasPorPersonas);
+        
+        String auto = "Ferrari";
+        double velocidad = 90.3;
+        System.err.printf("El auto %s se desplaza a %f km/h\n", auto, velocidad);
+    }
+    
+}
